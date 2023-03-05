9778a9495e96ab7bfb9729e1ef10adae7bcb6a8e
yubiyun <245817300@qq.com>
2023-03-05 10:56:19 +0800

    feat: module(cqhttp)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OpenaiModule } from './openai/openai.module';
+import { CqhttpModule } from './cqhttp/cqhttp.module';
 
 @Module({
-  imports: [OpenaiModule],
+  imports: [OpenaiModule, CqhttpModule],
   controllers: [AppController],
   providers: [AppService],
 })
