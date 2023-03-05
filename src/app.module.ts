33206f79cc38865c53d8e08185e2bcab4ef0ffc7
yubiyun <245817300@qq.com>
2023-03-05 11:00:04 +0800

    feat: module(chat)

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OpenaiModule } from './openai/openai.module';
 import { CqhttpModule } from './cqhttp/cqhttp.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [OpenaiModule, CqhttpModule],
+  imports: [OpenaiModule, CqhttpModule, ChatModule],
   controllers: [AppController],
   providers: [AppService],
 })
